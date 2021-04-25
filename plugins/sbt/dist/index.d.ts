8ccd4b21336ba2cf8d91f4a7bc12269d748f4830
Alexey Alekhin <alexey.alekhin@tapad.com>
2021-04-25 03:34:37 +0200

    add dist

---
+++ plugins/sbt/dist/index.d.ts
@@ -0,0 +1,17 @@
+import { Auto, IPlugin } from "@auto-it/core";
+import * as t from "io-ts";
+declare const pluginOptions: t.PartialC<{}>;
+export declare type ISbtPluginOptions = t.TypeOf<typeof pluginOptions>;
+/** Publish Scala projects with sbt */
+export default class SbtPlugin implements IPlugin {
+    /** The name of the plugin */
+    name: string;
+    /** The options of the plugin */
+    readonly options: ISbtPluginOptions;
+    /** Initialize the plugin with it's options */
+    constructor(options: ISbtPluginOptions);
+    /** Tap into auto plugin points. */
+    apply(auto: Auto): void;
+}
+export {};
+//# sourceMappingURL=index.d.ts.map
